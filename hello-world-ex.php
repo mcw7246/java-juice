f9d1dbc050e1150af4f61f705345385507142b26
mcw7246 <48811855+mcw7246@users.noreply.github.com>
2019-04-11 13:23:45 -0400

    Create hello-world-ex.php

---
+++ hello-world-ex.php
@@ -0,0 +1,15 @@
+<!--
+    FILE: hello-world-ex.php
+    DESC: Hello World page
+    AUTHOR: Mikayla Wishart
+    VERSION: 04/11/19
+    -->
+
+<?php
+
+    $pagename = "HelloWorld";
+    $pagenumber = 3;
+
+    include "assets/inc/content.php";
+
+?>
